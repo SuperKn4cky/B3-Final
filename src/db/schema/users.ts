89c20cb35f257b710a1bd48c7fc5d498518c247f
Warden <warmax7794@gmail.com>
2024-10-18 15:01:04 +0200

    users

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,6 +1,6 @@
 import { mysqlTable, int, varchar } from "drizzle-orm/mysql-core";
 export const users = mysqlTable("users", {
-  id: int().autoincrement(),
+  id: int().autoincrement().primaryKey(),
   name: varchar("255"),
   password: varchar("255"),
   mail: varchar("255"),
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import express from "express";
+import { users } from "./db/schema/users";
 
-const db = drizzle({ connection: { uri: process.env.DATABASE_URL } });
+async function startDatabase() {
+  try {
+    const db = drizzle({ connection: { uri: process.env.DATABASE_URL } });
+    const response = await db.select().from(users);
+    console.log(response);
+  } catch (error) {
+    console.error("Erreur lors de la connexion à la base de données :", error);
+  }
+}
 
 const app = express();
 const port = 3000;
