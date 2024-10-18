b2db8119ffbd2d92006e3e3d93a68f0cdec88dcc
SuperKnacky <superretards22@gmail.com>
2024-10-18 18:28:44 +0200

    db work

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import express from "express";
+import { fromError } from "zod-validation-error";
 
 export let db: ReturnType<typeof drizzle>;
 
 async function startDatabase() {
   try {
     db = drizzle({ connection: { uri: process.env.DATABASE_URL } });
+    await db.execute("select 1");
     console.log("connected to db");
   } catch (error) {
     console.error("Erreur lors de la connexion à la base de données :", error);
   }
 }
-
-startDatabase();
 
 export const app = express();
 const port = 3000;
