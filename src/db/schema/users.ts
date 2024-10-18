aa8d5c4cffa197f446ddcc6ad6cb487dbbd02f2f
SuperKnacky <superretards22@gmail.com>
2024-10-18 21:21:25 +0200

    secure

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -3,14 +3,15 @@ import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export const users = mysqlTable("users", {
   id: int().autoincrement().primaryKey(),
-  name: varchar("name", { length: 255 }),
-  password: varchar("password", { length: 255 }),
-  email: varchar("email", { length: 255 }),
+  name: varchar("name", { length: 255 }).notNull(),
+  password: varchar("password", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull(),
 });
 
 export const insertUserSchema = createInsertSchema(users, {
-  name: (schema) => schema.name.min(3).max(255),
+  name: (schema) => schema.name.min(3).max(255).regex(/^[a-zA-Z ]+$/),
   email: (schema) => schema.email.email(),
+  password: (schema) => schema.password.min(8).max(255).regex(/^[a-zA-Z0-9]+$/),
 });
 
 export const selectUserSchema = createSelectSchema(users, {
