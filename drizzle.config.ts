fce1f6748cf6e5a5c80eb165708284e5862d616a
Warden696 <warmax7794@gmail.com>
2024-10-18 19:18:59 +0200

    change drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,10 @@
+import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
+
 export default defineConfig({
-    dialect: "postgresql",
+    dialect: "mysql",
     schema: "./src/db/schema",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
 });
