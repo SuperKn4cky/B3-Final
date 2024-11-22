6445bee3eb996b45365c1a01944bcef881160e98
Quentin <superretards22@gmail.com>
2024-11-22 15:56:11 +0100

    change id matching to eq function

--- src/routes/users/delete.ts
+++ src/routes/users/delete.ts
@@ -1,20 +1,25 @@
 import { db, app } from "../../index";
-import { sql } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { users } from "../../db/schema/users";
 
 app.delete("/users/:id", async (req, res) => {
     try {
         const { id } = req.params;
-
-        const [result] = await db
-            .delete(users)
-            .where(sql`${users.id} = ${id}`)
-            .execute();
-
-        if (result.affectedRows === 0) {
-            res.status(404).json({ message: "User not found." });
+        const numericId = parseInt(id, 10);
+        if (isNaN(numericId)) {
+            res.status(400).json({ message: "Invalid user ID." });
         } else {
-            res.status(200).json({ message: "User successfully deleted." });
+
+            const [result] = await db
+                .delete(users)
+                .where(eq(users.id, numericId))
+                .execute();
+
+            if (result.affectedRows === 0) {
+                res.status(404).json({ message: "User not found." });
+            } else {
+                res.status(200).json({ message: "User successfully deleted." });
+            }
         }
     } catch (error) {
         console.error("Error deleting user:", error);
--- src/routes/users/get.ts
+++ src/routes/users/get.ts
@@ -1,5 +1,5 @@
 import { db, app } from "../../index";
-import { sql } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { users, selectUserSchema } from "../../db/schema/users";
 //import csurf from "csurf";
 
@@ -24,20 +24,25 @@ app.get("/users", async (req, res) => {
 app.get("/users/:id", async (req, res) => {
     try {
         const { id } = req.params;
-        const User = await db
-            .select()
-            .from(users)
-            .where(sql`${users.id} = ${id}`);
-        if (User.length === 0) {
-            res.status(404).json({
-                message: "User not found.",
-                user: `id: ${id}`,
-            });
+        const numericId = parseInt(id, 10);
+        if (isNaN(numericId)) {
+            res.status(400).json({ message: "Invalid user ID." });
         } else {
-            const validatedUser = User.map((user) => {
-                return selectUserSchema.parse(user);
-            });
-            res.status(200).json(validatedUser);
+            const User = await db
+                .select()
+                .from(users)
+                .where(eq(users.id, numericId));
+            if (User.length === 0) {
+                res.status(404).json({
+                    message: "User not found.",
+                    user: `id: ${id}`,
+                });
+            } else {
+                const validatedUser = User.map((user) => {
+                    return selectUserSchema.parse(user);
+                });
+                res.status(200).json(validatedUser);
+            }
         }
     } catch (error) {
         console.error("Error retrieving user:", error);
